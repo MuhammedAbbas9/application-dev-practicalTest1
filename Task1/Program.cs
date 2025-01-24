6419f75ec9c3118ea815ba9869f836c3c5d88f2e
Muhammed Abbas <shaarawy.abbas@gmail.com>
2025-01-23 23:48:01 -0500

    task1 initiated

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicalTest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("List of file operations, please select one option:");
+            Console.WriteLine("1.Read from file\n2.Write to file\n3.Count a character after read.\n4.Exit");
+            int choice = Console.Read();
+
+            switch (choice)
+            {
+                
+
+
+            }
+        }
+    }
+}
